c23af54e2526018bbe6b35448c21b7c92f486543
Yashila Ramesh <yashiramesh1122@gmail.com>
2022-11-12 15:10:29 -0500

    Fetch Nudges API client and serve

--- client/src/components/NudgeList/nudgeList.js
+++ client/src/components/NudgeList/nudgeList.js
@@ -1,5 +1,6 @@
 import './index.css';
 import { useState, useEffect, useCallback } from 'react';
 import { useDispatch, useSelector} from 'react-redux';
+import { fetchNudges } from '../../api/nudge';
 
-
+console.log(fetchNudges());
