6c70bdc37cc07227dde421ac29b9b9c8b00b827f
Andrew Zhao <steveand117@gmail.com>
2023-01-24 23:38:32 -0500

    Created Route for Create Nudge and updated schema for nudges
    - removed color
    - created is_active default to true
    - added comment field
    - added error sending/printing code
    - created POST route for nudge
    - created API function on frontend for easy access

--- client/src/pages/MainPage/index.js
+++ client/src/pages/MainPage/index.js
@@ -1,11 +1,11 @@
-import "./index.css";
-import PostingMenu from "../../components/PostingMenu";
-import PendingNudgeList from "../../components/PendingNudgeList";
-import { useDispatch } from "react-redux";
-import PopupModal from "../../components/PopupModal";
-import { useEffect, useState } from "react";
-import AssignMenu from "../../components/assignMenu";
-import { fetchNudges, createNudge } from "../../api/nudge";
+import './index.css';
+import PostingMenu from '../../components/PostingMenu'
+import PendingNudgeList from '../../components/PendingNudgeList';
+import { useDispatch } from 'react-redux';
+import PopupModal from '../../components/PopupModal';
+import {useEffect, useState} from 'react';
+import AssignMenu from '../../components/assignMenu';
+import { fetchNudges, createNudge } from '../../api/nudge';
 
 console.log(fetchNudges());
 
@@ -20,12 +20,13 @@ const MainPage = () => {
 
   function submitNudgeCreation() {
     createNudge({
-      message: "testing testing",
+      message: 'testing testing',
       date_created: Date(),
-      com_b: ["motivation", "capability"],
-    });
+      com_b: ['motivation', 'capability']
+      })
     // SOME LOGIC TO REFETCH THE NUDGE LIST -> Likely refetch nudges using api/nudge.js and update the redux state
   }
+
 
   useEffect(() => {
     fetchNudges()
@@ -39,57 +40,41 @@ const MainPage = () => {
   //<button onClick={() => {setShowModal(true); setCurrNudge(nudge)}}>assign</button>
   var count = 1;
   return (
-    <div className="MainPage">
-      {showModal && (
-        <PopupModal
-          content={
-            <AssignMenu
-              nudge={currNudge}
-              nudgeNum={1 + nudges.findIndex((obj) => obj === currNudge)}
-              setShowModal={setShowModal}
-            />
-          }
-          handleClose={() => {
-            setShowModal(!showModal);
-          }}
-        />
-      )}
+  <div className='MainPage'>
+    
+    {showModal && <PopupModal
+          content={<AssignMenu nudge={currNudge} nudgeNum={1 + nudges.findIndex((obj) => obj === currNudge)} setShowModal={setShowModal}/>}
+          handleClose={() => {setShowModal(!showModal);}}
+        />}
 
       <div className="NudgeList">
         <h2 className="NudgeListTitle">Nudge List</h2>
 
-        <button style={{ height: "3rem " }} onClick={submitNudgeCreation}>
-          Test Create Nudge using Preset Value{" "}
-        </button>
-        {nudges.map((nudge) => (
-          <div className="flex-container">
-            <div class="card">
-              <p>#{count++}</p>
-            </div>
-            <div class="card">
-              <p>{nudge.message}</p>
-            </div>
-            <div className="assignCard">
-              <p>{nudge.com_b.join(", ")} </p>
-
-              <button
-                className="assignButton"
-                onClick={() => {
-                  setShowModal(true);
-                  setCurrNudge(nudge);
-                }}
-              >
-                Assign
-              </button>
-            </div>
+      <div className= 'NudgeList'>
+      <h2 className="NudgeListTitle">Nudge List</h2>
+      
+      <button style={{height:"3rem "}} onClick={submitNudgeCreation}>Test Create Nudge using Preset Value </button>
+      {nudges.map((nudge) => 
+        
+        <div className="flex-container">
+          <div class="card">
+            <p>#{count++}</p>
           </div>
         ))}
       </div>
 
-      <div></div>
-      <PendingNudgeList />
-      <PostingMenu />
-    </div>
+            <button className="assignButton" onClick={() => {setShowModal(true); setCurrNudge(nudge);}}>Assign</button>
+          </div>
+        </div>)}
+        
+        </div>
+      
+    
+    
+    <div></div>
+    <PendingNudgeList/> 
+    <PostingMenu />
+  </div>
   );
 };
 
