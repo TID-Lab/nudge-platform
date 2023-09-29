3b1fd6fc8131e1113d87e9da53dd18cbea6531df
JSai S <jsai23@ipsec-10-2-65-22.vpn.gatech.edu>
2023-09-29 11:47:40 -0400

    adding auth users

--- server/src/sampleData.js
+++ server/src/sampleData.js
@@ -2,104 +2,123 @@
 const data = {
   authUserData: [
     {
-      username: 'jw199',
+      username: "jw199",
     },
     {
-      username: 'azhao63',
+      username: "azhao63",
     },
     {
-      username: 'jsomasundaram3',
+      username: "jsomasundaram3",
     },
     {
-      username: 'kchen430',
+      username: "kchen430",
+    },
+    {
+      username: "mb371",
+    },
+    {
+      username: "cpark333",
+    },
+    {
+      username: "rstarr7",
+    },
+    {
+      username: "rqian9",
     },
   ],
   participantData: [
     {
       participantId: 500,
-      labels: ['asian', 'female', '65+', 'tested', 'at-risk'],
+      labels: ["asian", "female", "65+", "tested", "at-risk"],
     },
     {
       participantId: 3,
-      labels: ['black', 'male', '30-40', 'untested', 'has-diabetes'],
+      labels: ["black", "male", "30-40", "untested", "has-diabetes"],
     },
     {
       participantId: 2,
-      labels: ['white', 'female', '18-29', 'untested', 'at-risk'],
+      labels: ["white", "female", "18-29", "untested", "at-risk"],
     },
     {
       participantId: 7,
-      labels: ['asian', 'female', '41-50', 'untested', 'caretaker'],
+      labels: ["asian", "female", "41-50", "untested", "caretaker"],
     },
     {
       participantId: 1,
-      labels: ['black', 'female', '65+', 'untested', 'has-diabetes'],
+      labels: ["black", "female", "65+", "untested", "has-diabetes"],
     },
     {
       participantId: 234,
-      labels: ['white', 'female', '18-29', 'untested', 'at-risk'],
+      labels: ["white", "female", "18-29", "untested", "at-risk"],
     },
     {
       participantId: 2346,
-      labels: ['latinx', 'male','30-40', 'untested', 'at-risk'],
+      labels: ["latinx", "male", "30-40", "untested", "at-risk"],
     },
     {
       participantId: 456,
-      labels: ['native-american', 'female', '18-29', 'untested', 'has-diabetes'],
+      labels: [
+        "native-american",
+        "female",
+        "18-29",
+        "untested",
+        "has-diabetes",
+      ],
     },
     {
       participantId: 9087,
-      labels: ['asian', 'female', '18-29', 'untested', 'at-risk'],
+      labels: ["asian", "female", "18-29", "untested", "at-risk"],
     },
     {
       participantId: 4523,
-      labels: ['latinx', 'male', '30-40', 'tested', 'caretaker'],
+      labels: ["latinx", "male", "30-40", "tested", "caretaker"],
     },
     {
       participantId: 32452,
-      labels: ['black', 'male', '41-50', 'untested', 'at-risk'],
+      labels: ["black", "male", "41-50", "untested", "at-risk"],
     },
     {
       participantId: 25,
-      labels: ['black', 'male', '51-64', 'tested', 'has-diabetes'],
+      labels: ["black", "male", "51-64", "tested", "has-diabetes"],
     },
     {
       participantId: 66,
-      labels: ['latinx', 'female', '41-50', 'untested', 'caretaker'],
+      labels: ["latinx", "female", "41-50", "untested", "caretaker"],
     },
     {
       participantId: 2341,
-      labels: ['asian', 'female', '18-29', 'untested', 'at-risk'],
+      labels: ["asian", "female", "18-29", "untested", "at-risk"],
     },
   ],
   nudgeData: [
     {
-      message: 'Hello world!',
+      message: "Hello world!",
       date_created: Date(),
-      com_b: ['O-SOC', 'C-PHY'],
+      com_b: ["O-SOC", "C-PHY"],
       is_active: true,
     },
     {
-      message: 'Make sure to test for the football game!',
+      message: "Make sure to test for the football game!",
       date_created: Date(),
-      com_b: ['O-SOC'],
+      com_b: ["O-SOC"],
       is_active: true,
     },
     {
-      message: 'Extra long nudge ~ lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere nisi nunc, sed sodales nunc vehicula ac. Maecenas et commodo nulla, non placerat urna. Aenean maximus tellus ligula, ut rhoncus libero facilisis at. ',
+      message:
+        "Extra long nudge ~ lorem ipsum Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam posuere nisi nunc, sed sodales nunc vehicula ac. Maecenas et commodo nulla, non placerat urna. Aenean maximus tellus ligula, ut rhoncus libero facilisis at. ",
       date_created: Date(),
       // com_b: ['M-REF'],
       is_active: true,
     },
     {
-      message: 'It would be a good time to use your at-home kit!',
+      message: "It would be a good time to use your at-home kit!",
       date_created: Date(),
-      com_b: ['C-PHY'],
+      com_b: ["C-PHY"],
       is_active: true,
     },
     {
-      message: 'Save your friends and family the hassle... test often!',
-      com_b: ['O-SOC', 'M-AUTO'],
+      message: "Save your friends and family the hassle... test often!",
+      com_b: ["O-SOC", "M-AUTO"],
       date_created: Date(),
     },
   ],
