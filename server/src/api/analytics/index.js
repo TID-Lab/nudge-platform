5335cc2befb504ee744759a8787a51434863e40e
Disha Aravind <daravind3@gatech.edu>
2023-12-12 02:20:42 -0500

    server api changes for weekly updates

--- server/src/api/analytics/index.js
+++ server/src/api/analytics/index.js
@@ -122,4 +122,42 @@ routes.get("/engagementcomments", async (req, res) => {
   }
 });
 
+routes.get("/weeklytopics", async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'text/csv');
+    const stream = fs.createReadStream('./data/updated_topics.csv');
+    stream.pipe(res);
+
+    stream.on('end', () => {
+      console.log('Stream ended successfully');
+    });
+
+    stream.on('error', (err) => {
+      console.error('Stream error:', err);
+    });
+  } catch (err) {
+    debug(`${err}`);
+    res.status(500).send(err);
+  }
+});
+
+routes.get("/weeklyposts", async (req, res) => {
+  try {
+    res.setHeader('Content-Type', 'text/csv');
+    const stream = fs.createReadStream('./data/updated_posts.csv');
+    stream.pipe(res);
+
+    stream.on('end', () => {
+      console.log('Stream ended successfully');
+    });
+
+    stream.on('error', (err) => {
+      console.error('Stream error:', err);
+    });
+  } catch (err) {
+    debug(`${err}`);
+    res.status(500).send(err);
+  }
+});
+
 module.exports = routes;
