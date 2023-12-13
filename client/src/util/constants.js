35eed8217bb84fee51a59102840b5f5ccf68837f
Kenneth Chen <efeichen@gmail.com>
2023-12-13 10:01:01 -0800

    Change upload participant format to Emory-compatible (#20)
    * Implement initial Emory CSV parsing
    
    * Fix bugs
    
    * Add logic for at-risk label

--- client/src/util/constants.js
+++ client/src/util/constants.js
@@ -6,3 +6,67 @@ export const CombColorMap = {
   "m-ref": "geekblue",
   "m-auto": "purple",
 };
+
+export const DEMO_ENUM = {
+  Age: ["18-29", "30-40", "41-50", "51-64", "65+"],
+  Diabetes: {
+    HasDiabetes: "has-diabetes",
+    PreDiabetes: "prediabetes",
+    FamHasDiabetes: "fam-diabetes",
+    AtRisk: "at-risk",
+    Caretaker: "caretaker",
+  },
+  Sex: {
+    Female: "female",
+    Male: "male",
+    Intersex: "intersex",
+    NoneApply: "none-apply",
+    NoAnswer: "no-answer",
+  },
+  Race: {
+    NativeAmerican: "native-american",
+    Black: "black",
+    Latinx: "latinx",
+    White: "white",
+    Asian: "asian",
+    PacificIslander: "pacific-islander",
+    Other: "other",
+    NoAnswer: "no-answer",
+  },
+};
+
+export const FIELD_ENUM = {
+  PreDiabetes: "scrn_predb_dx",
+  FamHasDiabetes: "scrn_fam_hx",
+  AtRisk: "scrn_risk",
+  AgeYrs: "age_yrs",
+  Race: "race_ethn_race",
+  Sex: "bio_sex_birth_2",
+};
+
+// Map of participant CSV headers to participant object keys on the nudge platform
+export const PARTICIPANT_CSV_MAP = {
+  record_id: "participantId",
+  scrn_db_dx: DEMO_ENUM.Diabetes.HasDiabetes,
+  scrn_predb_dx: DEMO_ENUM.Diabetes.PreDiabetes,
+  scrn_fam_hx: DEMO_ENUM.Diabetes.FamHasDiabetes,
+  scrn_risk: DEMO_ENUM.Diabetes.AtRisk,
+  scrn_cg: DEMO_ENUM.Diabetes.Caretaker,
+  bio_sex_birth_2: {
+    0: DEMO_ENUM.Sex.Male,
+    1: DEMO_ENUM.Sex.Female,
+    3: DEMO_ENUM.Sex.Intersex,
+    96: DEMO_ENUM.Sex.NoneApply,
+    99: DEMO_ENUM.Sex.NoAnswer,
+  },
+  race_ethn_race: {
+    1: DEMO_ENUM.Race.NativeAmerican,
+    2: DEMO_ENUM.Race.Black,
+    3: DEMO_ENUM.Race.Asian,
+    4: DEMO_ENUM.Race.PacificIslander,
+    5: DEMO_ENUM.Race.White,
+    15: DEMO_ENUM.Race.Other,
+    99: DEMO_ENUM.Race.NoAnswer,
+  },
+  race_ethn_hispanic: DEMO_ENUM.Race.Latinx,
+};
--- client/src/util/participant.js
+++ client/src/util/participant.js
@@ -1,47 +1,94 @@
+import { DEMO_ENUM, FIELD_ENUM, PARTICIPANT_CSV_MAP } from "./constants";
+
 /**
  *
  * @param {string} csv Convert participant CSV to JSON
  * @returns
  */
 export function participantCsvToJson(csv) {
-  const lines = csv.split("\n");
+  const lines = csv.replace(/(\r)/gm, "").split("\n");
   const headers = lines[0].split(",");
-  let active = false;
-  //using active\r as that is the return character and active will always be last
 
-  if (headers.includes("active\r") || headers.includes("active")) {
+  let active = false;
+
+  if (headers.includes("active")) {
     active = true;
   }
 
   const json = [];
 
   for (let i = 1; i < lines.length; i++) {
-    const participant = {};
-    const currentLine = lines[i].split(",");
+    const participant = {
+      labels: [],
+    };
+    const currLine = lines[i].split(",");
+    let isAtRisk = false; // at risk if any of the following are true: pre-diabetes, family history of diabetes, or at risk
 
     for (let j = 0; j < headers.length; j++) {
+      // first column is record_id (aka. participantId)
       if (j === 0) {
-        participant["participantId"] = currentLine[j];
-      } else if (active && j === headers.length - 1) {
-        if (
-          currentLine[j] !== "" &&
-          currentLine[j] !== "\r" &&
-          (currentLine[j] === "false\r" || currentLine[j] === "true\r")
-        ) {
-          participant["active"] = currentLine[j] === "true\r";
-        }
+        participant[PARTICIPANT_CSV_MAP.record_id] = currLine[j];
+      } else if (headers[j] === FIELD_ENUM.AgeYrs) {
+        // calculate age range
+        const age = parseInt(currLine[j]);
+
+        participant["labels"].push(calcAgeRange(age));
       } else {
-        if (currentLine[j] !== "" && currentLine[j] !== "\r") {
-          participant["labels"] = [
-            ...(participant["labels"] ?? []),
-            currentLine[j],
-          ];
+        // parse race labels
+        if (headers[j].includes(FIELD_ENUM.Race)) {
+          const [, raceCode] = headers[j].split("___");
+
+          if (currLine[j] === "1") {
+            participant["labels"].push(
+              PARTICIPANT_CSV_MAP.race_ethn_race[raceCode]
+            );
+          }
+        } else if (headers[j] === FIELD_ENUM.Sex) {
+          // parse sex labels
+          if (currLine[j] === "1") {
+            participant["labels"].push(
+              PARTICIPANT_CSV_MAP[headers[j]][currLine[j]]
+            );
+          }
+        } else if (
+          [
+            FIELD_ENUM.PreDiabetes,
+            FIELD_ENUM.FamHasDiabetes,
+            FIELD_ENUM.AtRisk,
+          ].includes(headers[j])
+        ) {
+          // parse diabetes at risk label
+          // at risk is true if any of the conditions are true
+          isAtRisk = isAtRisk || currLine[j] === "1";
+        } else {
+          // parse the rest of the binary labels
+          if (currLine[j] === "1") {
+            participant["labels"].push(PARTICIPANT_CSV_MAP[headers[j]]);
+          }
         }
       }
+    }
+
+    if (isAtRisk) {
+      participant["labels"].push(DEMO_ENUM.Diabetes.AtRisk);
     }
 
     json.push(participant);
   }
 
   return json;
+}
+
+function calcAgeRange(age) {
+  if (age >= 18 && age <= 29) {
+    return DEMO_ENUM.Age[0];
+  } else if (age >= 30 && age <= 40) {
+    return DEMO_ENUM.Age[1];
+  } else if (age >= 41 && age <= 50) {
+    return DEMO_ENUM.Age[2];
+  } else if (age >= 51 && age <= 64) {
+    return DEMO_ENUM.Age[3];
+  } else if (age >= 65) {
+    return DEMO_ENUM.Age[4];
+  }
 }
